bc66f08aa6916d00939a9c0e4a7f97183eefb613
Elifinanc <58734605+Elifinanc@users.noreply.github.com>
2020-02-17 15:06:53 +0100

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueteDesignPatternComposite
+{
+    public class Person
+    {
+        protected string Name { get; set; }
+        public List<Person> ChildrenList { get; } = new List<Person>();
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            string description = Name;
+            foreach (Person child in ChildrenList)
+            {
+                description += child;
+            }
+            return description;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace QueteDesignPatternComposite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("father");
+            Person child1 = new Person("mimi");
+            Person child2 = new Person("momo");
+            Person child3 = new Person("mumu");
+
+            person1.ChildrenList.Add(child1);
+            person1.ChildrenList.Add(child2);
+            person1.ChildrenList.Add(child3);
+
+            Console.WriteLine(person1);
+        }
+    }
+}
